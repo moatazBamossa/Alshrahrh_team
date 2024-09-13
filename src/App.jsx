34b5a065b5de0fb8e779a299f191a9af949c04a6
Moataz <moatazalbarbamossa@Moatazs-MacBook-Pro.local>
2024-09-14 00:45:16 +0300

    master commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <h1>هذا الموقع تابع لفريق الشراره الرياضي</h1>
+      <h2>قيد الصيانه</h2>
+    </div>
+  )
+}
+
+export default App
